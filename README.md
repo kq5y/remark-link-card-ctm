cb090b975954eacf4faad7094406f6deb9d9aee8
kq5y <tkser.edu@gmail.com>
2024-11-16 16:41:36 +0900

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# remark-link-card-ctm
+
+A rehype plugin to generate link cards inspired by [remark-link-card](https://github.com/gladevise/remark-link-card/)
+
+## Usage
+
+```shell
+pnpm add https://github.com/kq5y/remark-link-card-ctm
+```
+
+## License
+
+[MIT License](LICENSE)
---
+++ index.ts
@@ -0,0 +1,125 @@
+import type { Root } from "mdast";
+import { visit } from "unist-util-visit";
+import getOpenGraph from "open-graph-scraper";
+import { encode } from "he";
+
+interface RemarkLinkCardCtmOptions {
+  shortenUrl?: boolean;
+  imgAsyncLazy?: boolean;
+}
+
+interface Block {
+  url: string;
+  index: number;
+}
+
+interface ResultData {
+  title: string,
+  description: string,
+  faviconUrl: string,
+  ogImageSrc: string,
+  ogImageAlt: string,
+  hostname: string
+}
+
+function getFaviconUrl(url: string) {
+  return `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${url}&size=64`;
+}
+
+async function getOpenGraphResult(url: string) {
+  try {
+    const { result } = await getOpenGraph({url, timeout: 10000});
+    return result;
+  } catch (error) {
+    return undefined;
+  }
+}
+
+async function fetchData(url: string): Promise<ResultData> {
+  const ogResult = await getOpenGraphResult(url);
+  const parsedUrl = new URL(url);
+  const title = (
+    ogResult && ogResult.ogTitle && encode(ogResult.ogTitle) ||
+    parsedUrl.hostname
+  );
+  const description = (
+    ogResult && ogResult.ogDescription && encode(ogResult.ogDescription) ||
+    ""
+  );
+  const faviconUrl = getFaviconUrl(parsedUrl.hostname);
+  let ogImageSrc, ogImageAlt;
+  if(ogResult && ogResult.ogImage && ogResult.ogImage.length >= 1){
+    const ogImage = ogResult.ogImage[0];
+    ogImageSrc = ogImage.url;
+    ogImageAlt = (ogImage.alt && encode(ogImage.alt) || "");
+  } else {
+    ogImageSrc = "";
+    ogImageAlt = title;
+  }
+  return {
+    title, description, faviconUrl, ogImageSrc, ogImageAlt, hostname: parsedUrl.hostname
+  }
+}
+
+function generateHtml(url: string, data: ResultData, options: RemarkLinkCardCtmOptions): string {
+  const displayUrl = options.shortenUrl ? data.hostname : url;
+  return `
+    <a class="rlc-container" href="${url}">
+      <div class="rlc-info">
+        <div class="rlc-title">${data.title}</div>
+        <div class="rlc-description">${data.description}</div>
+        <div class="rlc-url-container">
+          <img
+            class="rlc-favicon"
+            src="${data.faviconUrl}"
+            alt="${data.title} favicon"
+            width="16"
+            height="16"
+            ${options.imgAsyncLazy ? `decoding="async" loading="lazy"` : ""}
+          />
+          <span class="rlc-url">${displayUrl}</span>
+        </div>
+      </div>
+      <div class="rlc-image-container">
+        <img
+          class="rlc-image"
+          src="${data.ogImageSrc}"
+          alt="${data.ogImageAlt}"
+          ${options.imgAsyncLazy ? `decoding="async" loading="lazy"` : ""}
+        />
+      </div>
+    </a>
+  `;
+}
+
+const remarkLinkCardCtm = (options: RemarkLinkCardCtmOptions = {}) => {
+  return async (tree: Root) => {
+    const blocks: Block[] = [];
+    visit(tree, "paragraph", (node, index) => {
+      if(node.children.length !== 1 || !node.data || !index){
+        return;
+      }
+      visit(node, "text", (textNode) => {
+        const urls = textNode.value.match(
+          /(https?:\/\/|www(?=\.))([-.\w]+)([^ \t\r\n]*)/g
+        );
+        if (urls && urls.length === 1) {
+          blocks.push({
+            url: urls[0],
+            index: index
+          });
+        }
+      })
+    });
+    for(const {url, index} of blocks){
+      const data = await fetchData(url);
+      const linkCardHtml = generateHtml(url, data, options);
+      tree.children.splice(index, 1, {
+        type: "html",
+        value: linkCardHtml
+      });
+    }
+  };
+};
+
+export default remarkLinkCardCtm;
