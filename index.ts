85e3ed904dbbf535db3c8a245578722fe1f8ad2c
kq5y <tkser.edu@gmail.com>
2025-04-22 23:05:33 +0900

    format

--- index.ts
+++ index.ts
@@ -1,69 +1,75 @@
+import he from "he";
+import getOpenGraph from "open-graph-scraper";
 import type { Literal, Parent } from "unist";
 import visit from "unist-util-visit";
-import getOpenGraph from "open-graph-scraper";
-import he from "he";
 
 interface RemarkLinkCardCtmOptions {
-  shortenUrl?: boolean;
-  imgAsyncLazy?: boolean;
+	shortenUrl?: boolean;
+	imgAsyncLazy?: boolean;
 }
 
 interface Block {
-  url: string;
-  index: number;
+	url: string;
+	index: number;
 }
 
 interface ResultData {
-  title: string,
-  description: string,
-  faviconUrl: string,
-  ogImageSrc: string,
-  ogImageAlt: string,
-  hostname: string
+	title: string;
+	description: string;
+	faviconUrl: string;
+	ogImageSrc: string;
+	ogImageAlt: string;
+	hostname: string;
 }
 
 function getFaviconUrl(url: string) {
-  return `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${url}&size=64`;
+	return `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${url}&size=64`;
 }
 
 async function getOpenGraphResult(url: string) {
-  try {
-    const { result } = await getOpenGraph({url, timeout: 10000});
-    return result;
-  } catch (error) {
-    return undefined;
-  }
+	try {
+		const { result } = await getOpenGraph({ url, timeout: 10000 });
+		return result;
+	} catch (error) {
+		return undefined;
+	}
 }
 
 async function fetchData(url: string): Promise<ResultData> {
-  const ogResult = await getOpenGraphResult(url);
-  const parsedUrl = new URL(url);
-  const title = (
-    ogResult && ogResult.ogTitle && he.encode(ogResult.ogTitle) ||
-    parsedUrl.hostname
-  );
-  const description = (
-    ogResult && ogResult.ogDescription && he.encode(ogResult.ogDescription) ||
-    ""
-  );
-  const faviconUrl = getFaviconUrl(url);
-  let ogImageSrc, ogImageAlt;
-  if(ogResult && ogResult.ogImage && ogResult.ogImage.length >= 1){
-    const ogImage = ogResult.ogImage[0];
-    ogImageSrc = ogImage.url;
-    ogImageAlt = (ogImage.alt && he.encode(ogImage.alt) || "");
-  } else {
-    ogImageSrc = "";
-    ogImageAlt = title;
-  }
-  return {
-    title, description, faviconUrl, ogImageSrc, ogImageAlt, hostname: parsedUrl.hostname
-  }
+	const ogResult = await getOpenGraphResult(url);
+	const parsedUrl = new URL(url);
+	const title =
+		(ogResult?.ogTitle && he.encode(ogResult.ogTitle)) || parsedUrl.hostname;
+	const description =
+		(ogResult?.ogDescription && he.encode(ogResult.ogDescription)) || "";
+	const faviconUrl = getFaviconUrl(url);
+	let ogImageSrc: string;
+	let ogImageAlt: string;
+	if (ogResult?.ogImage && ogResult.ogImage.length >= 1) {
+		const ogImage = ogResult.ogImage[0];
+		ogImageSrc = ogImage.url;
+		ogImageAlt = (ogImage.alt && he.encode(ogImage.alt)) || "";
+	} else {
+		ogImageSrc = "";
+		ogImageAlt = title;
+	}
+	return {
+		title,
+		description,
+		faviconUrl,
+		ogImageSrc,
+		ogImageAlt,
+		hostname: parsedUrl.hostname,
+	};
 }
 
-function generateHtml(url: string, data: ResultData, options: RemarkLinkCardCtmOptions): string {
-  const displayUrl = decodeURI(options.shortenUrl ? data.hostname : url);
-  return `
+function generateHtml(
+	url: string,
+	data: ResultData,
+	options: RemarkLinkCardCtmOptions,
+): string {
+	const displayUrl = decodeURI(options.shortenUrl ? data.hostname : url);
+	return `
     <a class="rlc-container" href="${url}">
       <div class="rlc-info">
         <div class="rlc-title">${data.title}</div>
@@ -93,37 +99,37 @@ function generateHtml(url: string, data: ResultData, options: RemarkLinkCardCtmO
 }
 
 const remarkLinkCardCtm = (options: RemarkLinkCardCtmOptions = {}) => {
-  return async (tree: Parent<Literal>) => {
-    const blocks: Block[] = [];
-    visit<Parent>(tree, "paragraph", (node, index) => {
-      if(node.children.length !== 1){
-        return;
-      }
-      if(node.data !== undefined){
-        return;
-      }
-      visit<Literal<string>>(node, "text", (textNode) => {
-        const urls = textNode.value.match(
-          /(https?:\/\/|www(?=\.))([-.\w]+)([^ \t\r\n]*)/g
-        );
-        if (urls && urls.length === 1) {
-          blocks.push({
-            url: urls[0],
-            index: index
-          });
-        }
-      })
-    });
-    for(const {url, index} of blocks){
-      const data = await fetchData(url);
-      const linkCardHtml = generateHtml(url, data, options);
-      tree.children.splice(index, 1, {
-        type: "html",
-        value: linkCardHtml
-      });
-    }
-    return tree;
-  };
+	return async (tree: Parent<Literal>) => {
+		const blocks: Block[] = [];
+		visit<Parent>(tree, "paragraph", (node, index) => {
+			if (node.children.length !== 1) {
+				return;
+			}
+			if (node.data !== undefined) {
+				return;
+			}
+			visit<Literal<string>>(node, "text", (textNode) => {
+				const urls = textNode.value.match(
+					/(https?:\/\/|www(?=\.))([-.\w]+)([^ \t\r\n]*)/g,
+				);
+				if (urls && urls.length === 1) {
+					blocks.push({
+						url: urls[0],
+						index: index,
+					});
+				}
+			});
+		});
+		for (const { url, index } of blocks) {
+			const data = await fetchData(url);
+			const linkCardHtml = generateHtml(url, data, options);
+			tree.children.splice(index, 1, {
+				type: "html",
+				value: linkCardHtml,
+			});
+		}
+		return tree;
+	};
 };
 
 export default remarkLinkCardCtm;
